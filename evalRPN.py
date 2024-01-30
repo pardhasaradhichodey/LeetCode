0bdffa75311b5d73d6945479cc4a020a525a0b41
pardhasaradhichodey <pardhu.dhoni@gmail.com>
2024-01-29 19:42:43 -0800

    1/29/2024

---
+++ evalRPN.py
@@ -0,0 +1,22 @@
+class Solution:
+    def resolves(self, a, b, Operator):
+        if Operator == '+':
+            return a + b
+        elif Operator == '-':
+            return a - b
+        elif Operator == '*':
+            return a * b
+        return int(a / b)
+
+    def evalRPN(self, tokens):
+        stack = []
+        for token in tokens:
+            if len(token) == 1 and ord(token) < 48:
+                integer2 = stack.pop()
+                integer1 = stack.pop()
+                operator = token
+                resolved_ans = self.resolves(integer1, integer2, operator)
+                stack.append(resolved_ans)
+            else:
+                stack.append(int(token))
+        return stack.pop()
